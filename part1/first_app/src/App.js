a656a06bea66824b6592192ade73d792b7c75230
andreygorshkov96 <89263877+andreygorshkov96@users.noreply.github.com>
2022-10-20 22:11:30 +0400

    Task 1.2 done. Had to declare variables globally in order to accsess them in nested function.

--- part1/first_app/src/App.js
+++ part1/first_app/src/App.js
@@ -1,3 +1,11 @@
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+
 const Header = (props) => {
   return(
     <h1>
@@ -6,11 +14,21 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
-  return(
+const Part = (props) => {
+  return  (
     <p>
-      {props.part} {props.exercises}
+    {props.name} {props.exercises}
     </p>
+  )
+}
+
+const Content = () => {
+  return(
+    <div>
+      <Part name = {part1} exercises = {exercises1}/>
+      <Part name = {part2} exercises = {exercises2}/>
+      <Part name = {part3} exercises = {exercises3}/>
+    </div>
   )
 }
 
@@ -24,23 +42,12 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
 
   return (
     <div>
         <Header course = {course}/>
-        <Content part = {part1}
-         exercises = {exercises1} />
-        <Content part = {part2}
-        exercises = {exercises2} />
-        <Content part = {part3}
-        exercises = {exercises3} />
+        <Content/>
         <Total total = {exercises1 + exercises2 + exercises3}/>
     </div>
   )
