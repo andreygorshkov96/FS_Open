ec611238b1c6441230c7046a25bb949411da1a54
andreygorshkov96 <89263877+andreygorshkov96@users.noreply.github.com>
2022-10-21 15:19:26 +0400

    Exercises 1.4 completed

--- part1/first_app/src/App.js
+++ part1/first_app/src/App.js
@@ -1,25 +1,32 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <h1>{course}</h1>
-      <p>{part1.name} {part1.exercises}</p>
-      <p>{part2.name} {part2.exercises}</p>
-      <p>{part3.name} {part3.exercises}</p>
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+      {parts.map(element => {
+        return(
+          <p> {element.name} {element.exercises} </p>
+        )
+      })}
+      <p>Nubmer of exercises: {parts.reduce((accamulator, object) => {
+        return accamulator + object.exercises;
+        }, 0)}
+      </p>
     </div>
   )
 }
